dd9bbc92f1a218d6c7e3829113015c87a9eb7e1c
Jana Kasova <ja.kasova@seznam.cz>
2018-12-07 15:40:37 +0000

    Search moved from root App to SearchNav component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React from "react";
 import "./App.css";
-import WeatherApp from "./WeatherApp.js";
+import SearchNav from "./SearchNav";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="navbar navbar-light bg-light rounded">
-        <span className="navbar-brand">Surfwhere Weather</span>
-
-        <form className="form-inline text-right" id="weather__form">
-          <button id="weather-refresh" className="btn btn-info mr-sm-2">
-            <i className="fas fa-location-arrow" />
-          </button>
-          <input
-            className="form-control mr-sm-2"
-            id="weather__form-location"
-            type="search"
-            placeholder="Enter a place..."
-            aria-label="Search"
-          />
-          <button className="btn btn-outline-info my-2 my-sm-0" type="submit">
-            <span id="Searchbutton">Search</span>
-          </button>
-        </form>
+      <div>
+        <SearchNav />
       </div>
     );
   }
---
+++ src/SearchNav.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class SearchNav extends Component {
+  render() {
+    return (
+      <div className="navbar navbar-light bg-light rounded">
+        <span className="navbar-brand">Surfwhere Weather</span>
+
+        <form className="form-inline text-right" id="weather__form">
+          <button id="weather-refresh" className="btn btn-info mr-sm-2">
+            <i className="fas fa-location-arrow" />
+          </button>
+          <input
+            className="form-control mr-sm-2"
+            id="weather__form-location"
+            type="search"
+            placeholder="Enter a place..."
+            aria-label="Search"
+          />
+          <button className="btn btn-outline-info my-2 my-sm-0" type="submit">
+            <span id="Searchbutton">Search</span>
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchNav;
