62933a551f5e690da0f157c7023f18bce68aaa01
Jana Kasova <ja.kasova@seznam.cz>
2018-12-20 15:33:12 +0100

    Added Forecast UI

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,45 +1,9 @@
 import React, { Component } from "react";
-import axios from "axios";
-import Api from "./Api";
+
 import App from "./App";
 import WeatherIcon from "./WeatherIcon.js";
 
 class Forecast extends Component {
-  state = {
-    city: this.props.city
-  };
-  componentWillMount() {
-    this.refresh();
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.setState({ city: nextProps.city }, () => {
-      this.refresh();
-    });
-  }
-
-  refresh = () => {
-    let url = `${Api.url}/data/2.5/forecast?appid=${Api.key}&units=metric&q=${
-      this.state.city
-    }`;
-
-    axios.get(url).then(response => {
-      let forecast = response.data.list;
-      // The API returns the forecast every 3 hour, so we are exrtacting
-      // the weather for every 24 hours which 8 times 3 hour cycles
-      let dailyForecast = [7, 15, 23, 31, 39].map(index => {
-        return {
-          description: forecast[index].weather[0].main,
-          icon: forecast[index].weather[0].icon,
-          precipitation: Math.round(forecast[index].main.humidity) + "%",
-          temperature: Math.round(forecast[index].main.temp),
-          day: new DateUtil(new Date(forecast[index].dt * 1000)).day(true),
-          wind: Math.round(forecast[index].wind.speed) + "km/h"
-        };
-      });
-      this.setState({ forecast: dailyForecast });
-    });
-  };
   render() {
     return (
       <div>
